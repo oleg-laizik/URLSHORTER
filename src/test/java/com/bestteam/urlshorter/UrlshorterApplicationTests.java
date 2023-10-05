6eeb20ee63be7574f0a29aae72a29fb130f8287f
ig.basalt@gmail.com <ig.basalt@gmail.com>
2023-10-05 18:23:23 +0300

    init commit

---
+++ src/test/java/com/bestteam/urlshorter/UrlshorterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestteam.urlshorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshorterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
